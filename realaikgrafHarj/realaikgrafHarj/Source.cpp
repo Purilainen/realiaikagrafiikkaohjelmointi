19f3cc47626ec23b7c432a50addd89b3a0b6ef22
Purilainen <p.kosola@gmail.com>
2015-11-03 14:22:23 +0200

    working with matrix
    asd

--- realaikgrafHarj/realaikgrafHarj/Source.cpp
+++ realaikgrafHarj/realaikgrafHarj/Source.cpp
@@ -121,6 +121,119 @@ private:
 	float tempValue = 0;
 };
 
+template<class T> class matrix_T
+{
+public:
+	matrix_T(int _rows,int  _cols, const T& _initial)
+	{
+		matrix.resize(_rows);
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			matrix[i].resize(_cols, _initial);
+		}
+		rows = _rows;
+		cols = _cols;
+	}
+
+	matrix_T operator()(const int& row, const int& col) const
+	{
+		return matrix[row][col];
+	}
+	matrix_T operator+(const matrix_T &m) const
+	{
+		//Matriisien yhteenlasku
+
+		//Luodaan tulosmatriisi
+		matrix_T result(rows, cols, 0.0);
+		//käydään matriisit läpi ja tehdään laskutoimitukset
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				result(i, j) = matrix[i][j] + m(i, j);
+			}
+		}
+		//palautetaan tulosmatriisi
+		return result;
+	}
+	matrix_T operator*(const matrix_T &m) const
+	{
+		//Matriisien kertolasku
+		rows = m.rows;
+		cols = m.cols;
+		//Tulosmatriisi
+		matrix_T result(rows, cols, 0.0);
+
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				for (int k = 0; k < rows; k++)
+				{
+					result(i, j) += matrix[i][k] * m(k, j);
+				}
+			}
+		}
+		return result;
+	}
+	matrix_T operator*(const std::vector<T> &v) const
+	{
+		//Matriisin kertominen vektorilla
+		//Luodaan tulosvektori
+		std::vector<T> result(v.size(), 0.0);
+
+		if (result.size() != rows)
+		{
+			std::cout << "ERROR: vector size incompatible with this matrix";
+		}
+		else
+		{
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					result[i] = matrix[i][j] * v[j];
+				}
+			}
+		}
+		return result;
+	}
+
+
+	int GetRows()
+	{
+		return rows;
+	}
+	int GetCols()
+	{
+		return cols;
+	}
+	matrix_T<T> transpose()
+	{
+		matrix_T result(rows, cols, 0.0);
+
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				result(i, j) = matrix[j][i];
+			}
+		}
+		return result;
+	}
+	//check if ortogonal
+	//käänteismatriisin laskenta
+	// siirto, kierto, skaalaus
+	//Peri matriisi- ja vektoriluokkamallista 2d-homogeenisten koordinaattien --> 3D --> perpektiivimuunnoksen vaatimat matriisit
+	//kasittelyyn omat luokkansa ja toteuta matriisiin kierto, siirto ja skaalaus.
+
+private:
+	std::vector<std::vector<T>> matrix;
+	int rows;
+	int cols;
+
+};
+
 
 int main()
 {
@@ -150,6 +263,17 @@ int main()
 
 	vector1.GetNorm();
 
+	matrix_T<int> mat1(3, 3, 1.0);
+
+
+	for (int i = 0; i<mat1.GetRows(); i++) 
+	{
+		for (int j = 0; j<mat1.GetCols(); j++) 
+		{
+			std::cout << mat1(i,j) << ", ";
+		}
+		std::cout << std::endl;
+	}
 
 	system("pause");
 }
